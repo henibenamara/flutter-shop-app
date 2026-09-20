a88e8e3d5672b9651a7420a89a4e2809d7ba9eda
Heni BEN AMARA <63763172+henibenamara@users.noreply.github.com>
2026-09-20 22:57:02 +0100

    Add core and auth data/domain layers

---
+++ lib/features/auth/domain/entities/user.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  const User({
+    required this.id,
+    required this.username,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    this.imageUrl,
+  });
+
+  final int id;
+  final String username;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String? imageUrl;
+
+  String get fullName => '$firstName $lastName';
+
+  @override
+  List<Object?> get props => [id, username, firstName, lastName, email, imageUrl];
+}
